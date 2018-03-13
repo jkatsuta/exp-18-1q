2bcb1aaf675f2a012609e3d1381cacc81a28ef27
junichiro-katsuta <junichiro-katsuta@gmo.jp>
2018-03-13 15:31:58 +0900

    minor change

--- maddpg/maddpg/common/tf_util.py
+++ maddpg/maddpg/common/tf_util.py
@@ -21,7 +21,10 @@ def concatenate(arrs, axis=0):
 def argmax(x, axis=None):
     return tf.argmax(x, axis=axis)
 def softmax(x, axis=None):
-    return tf.nn.softmax(x, axis=axis)
+    try:
+        return tf.nn.softmax(x, axis=axis)
+    except TypeError:
+        return tf.nn.softmax(x, dim=axis)
 
 # ================================================================
 # Misc
