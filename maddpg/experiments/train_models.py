f48404d2c74520813b89e15a5a9859d3c3e04232
junichiro-katsuta <junichiro-katsuta@gmo.jp>
2018-03-16 09:48:11 +0900

    minor chnage

--- maddpg/experiments/train_models.py
+++ maddpg/experiments/train_models.py
@@ -5,8 +5,9 @@ import sys
 params = eval(open(sys.argv[1]).read())
 
 for scenario in params['scenarios']:
-    com = 'python train.py --scenario %s --num-episodes %d'\
+    com = 'python train.py --scenario %s --num-episodes %d '\
         % (scenario, params['num_episodes'])
+    com += '--max-episode-len %d ' % (params['max_episode_len'])
     if params['is_parallel']:
         com += ' &'
     os.system(com)
