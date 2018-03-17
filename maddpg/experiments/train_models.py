d1f8bac86d15f204d8acf538a787c3f755f0c796
junichiro-katsuta <junichiro-katsuta@gmo.jp>
2018-03-18 00:15:23 +0900

    minor update

--- maddpg/experiments/train_models.py
+++ maddpg/experiments/train_models.py
@@ -2,18 +2,31 @@
 import os
 import sys
 
-params = eval(open(sys.argv[1]).read())
 
-for scenario in params['scenarios']:
-    for i, num_episode in enumerate(params['num_episodes']):
-        com = 'python train.py --scenario %s ' % scenario
-        com += '--num-episodes %d ' % num_episode
-        if params.get('is_variable_max_episode_len', False):
-            com += '--dic-variable-max-episode-len "%s" '\
-                % str(params['par_variable_max_episode_len'])
-        else:
-            com += '--max-episode-len %s ' % params['max_episode_lens'][i]
-        if params['is_parallel']:
-            com += ' &'
-        os.system(com)
-        # print(com)
+def _train_model(i_epi, scenario, num_episode, params, dic_var_epi_len):
+    com = 'python train.py --scenario %s ' % scenario
+    com += '--num-episodes %d ' % num_episode
+    if len(dic_var_epi_len) > 0:
+        com += '--dic-variable-max-episode-len "%s" ' % str(dic_var_epi_len)
+    else:
+        com += '--max-episode-len %s ' % params['max_episode_lens'][i_epi]
+    if params['is_parallel']:
+        com += ' &'
+    os.system(com)
+    # print(com)
+
+
+def train_models(params):
+    dic_var_epi_lens = [{}]
+    if params.get('is_variable_max_episode_len', False):
+        dic_var_epi_lens = params['par_variable_max_episode_lens']
+
+    for scenario in params['scenarios']:
+        for dic_var_epi_len in dic_var_epi_lens:
+            for i, num_episode in enumerate(params['num_episodes']):
+                _train_model(i, scenario, num_episode, params, dic_var_epi_len)
+
+
+if __name__ == '__main__':
+    fn_param = sys.argv[1]
+    train_models(eval(open(fn_param).read()))
