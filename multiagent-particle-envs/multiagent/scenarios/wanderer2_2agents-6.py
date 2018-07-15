48dd469fecef42c791c165eb4a42191115aebb6e
junichiro-katsuta <junichiro-katsuta@gmo.jp>
2018-07-15 11:11:30 +0900

    add exp10 scenario

---
+++ multiagent-particle-envs/multiagent/scenarios/wanderer2_2agents-6.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import wanderer2
+
+
+class Scenario(wanderer2.Scenario):
+	def set_params(self):
+	    self.n_agents = 2
+	    self.PENALTY_WEIGHT = 0.01  # energy
+	    self.PENALTY_WEIGHT2 = 0.02  # attention
+	    self.VISIBLE_WEIGHT = 0.1
+	    self.L_PROB = 1.0
